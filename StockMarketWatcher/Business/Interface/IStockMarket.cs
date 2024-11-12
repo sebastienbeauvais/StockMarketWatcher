c841e75bee48c22029da6971a9a96d16d3fd6596
sebastienbeauvais <sbeauvais7@gmail.com>
2024-11-11 18:41:24 -0800

    Initial commit. Project setup

--- StockMarketWatcher/Business/Interface/IStockMarket.cs
+++ StockMarketWatcher/Business/Interface/IStockMarket.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StockMarketWatcher.Business.Interface
 {
-    internal interface IStockMarket
+    public interface IStockMarket
     {
     }
 }
--- StockMarketWatcher/Business/Interface/ISubscriber.cs
+++ StockMarketWatcher/Business/Interface/ISubscriber.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StockMarketWatcher.Business.Interface
 {
-    internal interface ISubscriber
+    public interface ISubscriber
     {
     }
 }
