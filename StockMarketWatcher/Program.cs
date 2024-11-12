161bedd6bf0fca186f27296bf598b5011879cb27
sebastienbeauvais <sbeauvais7@gmail.com>
2024-11-11 18:38:57 -0800

    Add project files.

---
+++ StockMarketWatcher/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = ConfigureServices();
+
+            var stockMarket = serviceProvider.GetRequiredService<StockMarketWatcher.StockMarketWatcher>();
+
+            stockMarket.StartWatching();
+        }
+        private static ServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            //Register Services
+
+            return services.BuildServiceProvider();
+        }
+
+    }
+}
---
+++ StockMarketWatcher/StockMarketWatcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarketWatcher
+{
+    public class StockMarketWatcher
+    {
+        public StockMarketWatcher() { }
+        public void StartWatching()
+        {
+
+        }
+    }
+}
